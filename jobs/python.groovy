1afe6c9ee3a6939d9d30c1e5397ba589af3cc826
Luis Vidaechea <vidaechea.luis@gmail.com>
2018-08-23 12:28:03 +0200

    add python.groovy

--- jobs/python.groovy
+++ jobs/python.groovy
@@ -2,7 +2,7 @@ project_name = "jenkins-pipeline-python"
 repo = "https://github.com/vidaechea/test_jenkins_1.git"
 repo_name = "repo"
 
-pipelineJOb(project_name) {
+pipelineJob(project_name) {
 	definition {
 		triggers {
 			scm('H/1 * * * *')
