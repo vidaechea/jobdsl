e2ed355243fe14551ea2b666dc05d2db009b1fa5
Luis Vidaechea <vidaechea.luis@gmail.com>
2018-08-23 12:32:09 +0200

    add test.groovy

---
+++ jobs/test.groovy
@@ -0,0 +1,23 @@
+project_name = "jenkins-pipeline-python"
+repo = "https://github.com/vidaechea/test_jenkins_1.git"
+repo_name = "repo"
+
+pipelineJob(project_name) {
+	definition {
+		triggers {
+			scm('H/1 * * * *')
+		}
+
+		cpsScm {
+			scm {
+				git {
+					remote {
+						name(repo_name)
+						url(repo)
+					}
+				}
+				scriptPath("Jenkinsfile")
+			}
+		}
+	}
+}
